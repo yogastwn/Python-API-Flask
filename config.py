f2a695f7159b13620a7ddf5f2464fb7f6d9110ca
yogastwn <yoga.stwn@students.amikom.ac.id>
2024-10-30 14:29:52 +0700

    test update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # Konfigurasi MySQL
 # app.config[ 'SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/db_flask_api'
-app.config[ 'SQLALCHEMY_DATABASE_URI'] = 'mysql://flaskapiv1_progressat:f0ffb3988e7eb72005ea2fd4e3874c0f4e92d131@hwkqo.h.filess.io:3307/flaskapiv1_progressat' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://flaskapiv1_progressat:f0ffb3988e7eb72005ea2fd4e3874c0f4e92d131@hwkqo.h.filess.io:3307/flaskapiv1_progressat' 
 
 app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
